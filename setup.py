87f720f2562b291f73e474982b436a27e1f58703
hccodetech <162390283+hccodetech@users.noreply.github.com>
2024-04-14 13:35:20 +0900

    Add files

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = "YAPCP",
+    version="0.0.0.1",
+    descriptor="Yet Another PyPI Calculator Practice",
+    author="hccodetech",
+    url = "https://github.com/hccodetech/yapcp",
+    download_url = "https://github.com/hccodetech/yapcp",
+    pacakages = [ 'yapcp' ],
+    classifiers = [
+        "Programming Language :: Python :: 3"
+    ]
+)
